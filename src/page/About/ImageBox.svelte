e289db4c7e4bef360617f73d5fa0cf7788247078
Donghyun Lee <erzen0805@naver.com>
2023-03-05 13:03:35 +0900

    ImgBox 레이아웃변경.

--- src/page/About/ImageBox.svelte
+++ src/page/About/ImageBox.svelte
@@ -3,12 +3,28 @@
   export let imgTitle;
 </script>
 
-<div style="width: 7vw; height: 9vw; background-color: red;">
+<div class="container">
   <img src={imgSource} alt={imgTitle}>
-  <div>{imgTitle}</div>
+  <div class="title">{imgTitle}</div>
 </div>
 
 <style>
 .container {
+  width: 6vw;
+  height: 8vw;
+  /* border: 3px solid green; */
+}
+img {
+  width: 100%;
+  height: 6vw;
+  border: 3px solid gold;
+  border-radius: 20%;
+}
+.title {
+  font-family: KoHo;
+  font-size: 1rem;
+  color: rgb(193, 193, 193);
+  text-align: center;
+  margin-top: 3px;
 }
 </style>
--- src/page/About/Right.svelte
+++ src/page/About/Right.svelte
@@ -11,22 +11,44 @@
     </div>
   </div>
   <div class="contents">
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
-    <ImageBox imgSource="asdfas" imgTitle="sadf" />
+    <ImageBox imgSource="./src/assets/c.png" imgTitle="C++" />
+    <ImageBox imgSource="./src/assets/mfc.png" imgTitle="MFC" />
+    <ImageBox imgSource="./src/assets/qt.png" imgTitle="QT C++" />
+    <ImageBox imgSource="./src/assets/dotnet.png" imgTitle="C#" />
+    <ImageBox imgSource="./src/assets/wpf.png" imgTitle="WPF" />
+    <ImageBox imgSource="./src/assets/dotnetcore.png" imgTitle=".NET CORE" />
+    <ImageBox imgSource="./src/assets/java.png" imgTitle="Java" />
+    <ImageBox imgSource="./src/assets/go.png" imgTitle="GO" />
+    <ImageBox imgSource="./src/assets/python.png" imgTitle="Python" />
+    <ImageBox imgSource="./src/assets/jupyter_notebook.png" imgTitle="J-Notebook" />
+    <ImageBox imgSource="./src/assets/keras.png" imgTitle="Keras" />
+
+    <ImageBox imgSource="./src/assets/html.png" imgTitle="HTML5" />
+    <ImageBox imgSource="./src/assets/css.png" imgTitle="CSS" />
+    <ImageBox imgSource="./src/assets/js.png" imgTitle="JavaScript" />
+    <ImageBox imgSource="./src/assets/jquery.png" imgTitle="jQuery" />
+    <ImageBox imgSource="./src/assets/es6.png" imgTitle="ES6" />
+    <ImageBox imgSource="./src/assets/svelte.png" imgTitle="Svelte.js" />
+    <ImageBox imgSource="./src/assets/react.png" imgTitle="React.js" />
+    <ImageBox imgSource="./src/assets/vue.png" imgTitle="Vue.js" />
+    <ImageBox imgSource="./src/assets/vite.png" imgTitle="Vite" />
+
+    <ImageBox imgSource="./src/assets/github.png" imgTitle="GitHub" />
+    <ImageBox imgSource="./src/assets/bitbucket.png" imgTitle="Bitbucket" />
+
+    <ImageBox imgSource="./src/assets/redmine.png" imgTitle="Redmine" />
+    <ImageBox imgSource="./src/assets/git.png" imgTitle="Git" />
+    <ImageBox imgSource="./src/assets/iec61850.png" imgTitle="IEC61850" />
+    <ImageBox imgSource="./src/assets/jira.png" imgTitle="Jira" />
+    <ImageBox imgSource="./src/assets/modbus.png" imgTitle="MODBUS" />
+    <ImageBox imgSource="./src/assets/mssql.png" imgTitle="MSSQL" />
+    <ImageBox imgSource="./src/assets/mysql.png" imgTitle="MySQL" />
+    <ImageBox imgSource="./src/assets/notion.png" imgTitle="Notion" />
+    <ImageBox imgSource="./src/assets/oracle.png" imgTitle="OracleDB" />
+    <ImageBox imgSource="./src/assets/redis.png" imgTitle="Redis" />
+    <ImageBox imgSource="./src/assets/sqlite.png" imgTitle="SQLite" />
+    <ImageBox imgSource="./src/assets/svn.png" imgTitle="SVN" />
+    <ImageBox imgSource="./src/assets/tcp.png" imgTitle="TCP/IP" />
   </div>
 </section>
 
@@ -55,7 +77,7 @@
   z-index: 1;
   position: relative;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   /* border: 3px solid green; */
   width: 90%;
   height: calc(100vh - 310px);
