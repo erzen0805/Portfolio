9dc15033b93f13a839bea3deb39b97fa9ded7337
Donghyun Lee <erzen0805@naver.com>
2023-07-08 10:12:16 +0900

    N/A

--- src/page/About/CollaborationTools.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import ImageBox from './ImageBox.svelte';
-</script>
-
-<ImageBox source="img/jira.png" title="Jira" />
-<ImageBox source="img/redmine.png" title="Redmine" />
-<ImageBox source="img/notion.png" title="Notion" />
-<ImageBox source="img/git.png" title="Git" />
-<ImageBox source="img/svn.png" title="SVN" />
