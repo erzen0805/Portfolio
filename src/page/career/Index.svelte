13002ade886f578adbed2b43d5f3ba1da42096b2
Donghyun Lee <erzen0805@naver.com>
2023-07-09 10:03:14 +0900

    career - image, 임시템플릿 삽입

--- src/page/career/Index.svelte
+++ src/page/career/Index.svelte
@@ -1,6 +1,12 @@
 <script>
   import { fly } from 'svelte/transition';
   import { inview } from 'svelte-inview';
+  import P0 from './P0.svelte';
+  import P1 from './P1.svelte';
+  import P2 from './P2.svelte';
+  import P3 from './P3.svelte';
+  import P4 from './P4.svelte';
+  import P5 from './P5.svelte';
   import P6 from './P6.svelte';
   import P7 from './P7.svelte';
   import P8 from './P8.svelte';
@@ -26,7 +32,7 @@
     {title: "~2021 (1.0년)", company: "SCADA | Autonics"},
     {title: "~2020 (1.2년)", company: "카카오 선물하기 서버 | 쿠프마케팅"},
     {title: "~2019 (0.5년)", company: "가상화폐 자동투자 봇 | 개인개발"},
-    {title: "~2018 (1.3년)", company: "ATM/KIOSK 관리 서버 | 효성T&S"},
+    {title: "~2018 (1.3년)", company: "ATM/KIOSK 관리 서버 | 효성TNS"},
     {title: "~2017 (7.3년)", company: "전력설비 예방진단 | 유호전기공업"},
     {title: "~2014 (0.8년)", company: "전력 SCADA | 가보"},
     {title: "~2009 (1.0년)", company: "바다이야기 | 엔톤"},
@@ -39,14 +45,26 @@
   <div style="width: calc(100% - 500px); margin-bottom: 50px;">
   {#if isInView}
     <div class="contents" in:fly={{ y: 200, duration: 1000 }}>
-      {#if viewPage === 0}
+      {#if viewPage === 9}
         <P9/>
-      {:else if viewPage === 1}
+      {:else if viewPage === 8}
         <P8/>
-      {:else if viewPage === 2}
+      {:else if viewPage === 7}
         <P7/>
-      {:else if viewPage === 3}
+      {:else if viewPage === 6}
         <P6/>
+      {:else if viewPage === 5}
+        <P5/>
+      {:else if viewPage === 4}
+        <P4/>
+      {:else if viewPage === 3}
+        <P3/>
+      {:else if viewPage === 2}
+        <P2/>
+      {:else if viewPage === 1}
+        <P1/>
+      {:else if viewPage === 0}
+        <P0/>
       {:else}
         <UnderConstruction />
       {/if}
@@ -56,7 +74,7 @@
   <div style="width: 500px;">
     <ul class="list">
       {#each projects as { title, company }, i}
-      <li class:selected={viewPage===i} on:click|preventDefault={()=>onChangePage(i)}>
+      <li class:selected={viewPage===projects.length-i-1} on:click|preventDefault={()=>onChangePage(projects.length-i-1)}>
         <span class="label-left">{title}</span><span class="label-right">{company}</span>
       </li>
       {/each}
---
+++ src/page/career/P4.svelte
@@ -0,0 +1,74 @@
+<script>
+</script>
+
+<div class="root">
+    <h3 class="project-name">ATM/KIOSK 관리 서버</h3>
+    <img src="img/career/atms_hyosung.jpg" style="width: 100%; border-radius: 10px;"/>
+    <h4 class="title">프로젝트 설명</h4>
+    <div class="content">
+      <p class="contents-normal">
+        카카오 선물하기 서버는 카카오톡을 통해 다른 사용자에게 선물을 보낼 수 있는 기능을 제공합니다.
+      </p>
+      <p class="contents-normal">
+        이를 위해 파트너사와 통신 연동을 통해 쿠폰 발급, 연장, 폐기 서비스를 신속하고 신뢰성 있게 제공하는 것이 핵심입니다.
+      </p>
+      <p class="contents-normal">
+        As-Is 발급서비스와 비교하여 발급 속도을 개선하고, 운영자가 서버 운영 상황을 모니터링 및 제어가능하도록 개선하였습니다.
+      </p>
+      <p class="contents-normal">
+        또한 서버 이상 발생 시 생기는 대량 장애 메시지를 벌크동작으로 만들고 통계정보를 추가하여, 빠른 장애 복구 할 수 있게 하였습니다.
+      </p>
+      <span class="contents-strong">리뉴얼프로젝트 | 2인(Senior, Junior) | Senior 개발자로 참여</span>
+    </div>
+    <h4 class="title">수행 업무</h4>
+    <div class="content">
+      <p class="contents-normal">
+        화면기획, 발급/선발급 서버, 운영자용 웹서버 개발
+      </p>
+      <p class="contents-strong">
+        언어 - Java Spring Boot | Vue.js
+      </p>
+      <p class="contents-strong">
+        기타 - HTTP | Oracle
+      </p>
+    </div>
+</div>
+
+<style>
+.root {
+  padding: 20px 20px 40px 20px;
+  font-family: KoHo;
+  font-size: 1.25rem;
+  font-weight: 700;
+  color: rgb(146, 146, 146);
+}
+.project-name {
+  font-size: 1.8rem;
+  font-weight: 700;
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+  color: rgb(184, 232, 41);
+}
+.title {
+  font-size: 1.2rem;
+  font-weight: 600;
+  color: white;
+  margin: 40px 0 0 20px;
+}
+.content {
+  margin: 20px 10px 0px 10px;
+}
+.contents-strong {
+  color: rgb(146, 146, 146);
+  font-weight: 700;
+  font-size: 1.1rem;
+  line-height: 1.7;
+}
+.contents-normal {
+  font-size: 1.05rem;
+  font-weight: 400;
+  color: rgb(213, 213, 213);
+  line-height: 1.7;
+}
+</style>
